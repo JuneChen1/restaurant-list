050f2c0b5028ae29958dba423875f1d286d33948
june850713@gmail.com <june850713@gmail.com>
2022-05-23 14:29:12 +0800

    add seeder function

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Restaurant = require('../restaurant')
+const restaurants = require('../../restaurant.json').results
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+  Restaurant.create(restaurants)
+    .then(() => {
+      console.log('seeds created')
+    })
+    .catch(error => {console.log(error)})
+})
