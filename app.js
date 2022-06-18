6ac40c960086e5b72842eaf6f9405d78fbaa9b68
june850713@gmail.com <june850713@gmail.com>
2022-06-18 15:31:01 +0800

    add message partials

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 3000
 const exphts = require('express-handlebars')
 const bodyParser = require('body-parser')
 const usePassport = require('./config/passport')
+const flash = require('connect-flash')
 
 const routes = require('./routes')
 require('./config/mongoose')
@@ -23,10 +24,13 @@ app.use(session({
 }))
 
 usePassport(app)
+app.use(flash())
 
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,9 @@
 module.exports = {
   authenticator: (req, res, next) => {
-    console.log(req.isAuthenticated())
     if (req.isAuthenticated()) {
       return next()
     }
+    req.flash('warning_msg', '請先登入才能使用')
     res.redirect('/users/login')
   }
 }
